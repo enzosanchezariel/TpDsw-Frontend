34655901fc98fde28d16218348e9089f0b8bf6a9
Enzo Ariel <enzo.sanchez.ariel@gmail.com>
2025-02-14 19:26:54 -0300

    Test end to end pedido

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    specPattern: ["cypress/integration/**/*.cy.{js,ts}", "cypress/e2e/**/*.cy.{js,ts}"],
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
